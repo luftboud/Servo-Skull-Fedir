486cb729af3cde5d65650e0d5f2c7daea4c549a2
Vlad Danylyshyn <vladuslavsd@gmail.com>
2025-11-06 22:16:50 +0200

    Integrate mp3 file response

--- src/llm/llm_server.py
+++ src/llm/llm_server.py
@@ -1,5 +1,7 @@
 import pydantic
+from gtts import gTTS
 from fastapi import FastAPI
+import uuid
 
 from llm import WarhammerBrain
 
@@ -8,18 +10,24 @@ class LLMInputModel(pydantic.BaseModel):
     user_prompt: str
 
 
-class LLMOutputModel(pydantic.BaseModel):
-    response: str
-
 app = FastAPI()
 brain_llm = WarhammerBrain()
 
 
+@app.post('/ask_llm_mp3')
+def ask_llm_mp3(request: LLMInputModel):
+    response = brain_llm.process_prompt(request.user_prompt)
+    tts = gTTS(response)
+    name = str(uuid.uuid4())[:8]
+    filename = f'../llm_launcher/mp3_output/{name}.mp3'
+    tts.save(filename)
+
+    return filename
+
 @app.post('/ask_llm')
 async def ask_llm(request: LLMInputModel):
     response = brain_llm.process_prompt(request.user_prompt)
-    output = LLMOutputModel(response=response)
-    return output
+    return response
 
 
 if __name__ == "__main__":
